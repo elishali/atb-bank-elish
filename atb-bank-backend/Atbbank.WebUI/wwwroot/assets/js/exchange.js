0b79d68be99cdba2b35b04681bcc6393ca10a6ef
elishali <125192504+elishali@users.noreply.github.com>
2024-01-20 01:02:45 +0400

    database

--- atb-bank-backend/Atbbank.WebUI/wwwroot/assets/js/exchange.js
+++ atb-bank-backend/Atbbank.WebUI/wwwroot/assets/js/exchange.js
@@ -1,40 +1,40 @@
-// const currency_calculationEl_one = document.getElementById('currency_calculation-one');
-// const amountEl_one = document.getElementById('amount-one');
-// const currency_calculationEl_two = document.getElementById('currency_calculation-two');
-// const amountEl_two = document.getElementById('amount-two');
+ const currency_calculationEl_one = document.getElementById('currency_calculation-one');
+ const amountEl_one = document.getElementById('amount-one');
+ const currency_calculationEl_two = document.getElementById('currency_calculation-two');
+ const amountEl_two = document.getElementById('amount-two');
 
-// const rateEl = document.getElementById('rate');
-// const swap = document.getElementById('swap');
+ const rateEl = document.getElementById('rate');
+ const swap = document.getElementById('swap');
 
-// // Fetch exchange rates and update the DOM
-// function calculate() {
-//   const currency_calculation_one = currency_calculationEl_one.value;
-//   const currency_calculation_two = currency_calculationEl_two.value;
+ // Fetch exchange rates and update the DOM
+ function calculate() {
+   const currency_calculation_one = currency_calculationEl_one.value;
+   const currency_calculation_two = currency_calculationEl_two.value;
 
-//   fetch(`https://api.exchangerate-api.com/v4/latest/${currency_calculation_one}`)
-//     .then(res => res.json())
-//     .then(data => {
-//       // console.log(data);
-//       const rate = data.rates[currency_calculation_two];
+   fetch(`https://api.exchangerate-api.com/v4/latest/${currency_calculation_one}`)
+     .then(res => res.json())
+     .then(data => {
+       // console.log(data);
+       const rate = data.rates[currency_calculation_two];
       
-//       rateEl.innerText = `1 ${currency_calculation_one} = ${rate} ${currency_calculation_two}`;
+       rateEl.innerText = `1 ${currency_calculation_one} = ${rate} ${currency_calculation_two}`;
 
-//       amountEl_two.value = (amountEl_one.value * rate).toFixed(2)
-//     })
+       amountEl_two.value = (amountEl_one.value * rate).toFixed(2)
+     })
 
-// }
+ }
 
-// // Event listeners
-// currency_calculationEl_one.addEventListener('change', calculate);
-// amountEl_one.addEventListener('input', calculate);
-// currency_calculationEl_two.addEventListener('change', calculate);
-// amountEl_two.addEventListener('input', calculate);
+ // Event listeners
+ currency_calculationEl_one.addEventListener('change', calculate);
+ amountEl_one.addEventListener('input', calculate);
+ currency_calculationEl_two.addEventListener('change', calculate);
+ amountEl_two.addEventListener('input', calculate);
 
-// swap.addEventListener('click', ()=> {
-//   const temp = currency_calculationEl_one.value;
-//   currency_calculationEl_one.value = currency_calculationEl_two.value;
-//   currency_calculationEl_two.value = temp;
-//   calculate()
-// })
+ swap.addEventListener('click', ()=> {
+   const temp = currency_calculationEl_one.value;
+   currency_calculationEl_one.value = currency_calculationEl_two.value;
+   currency_calculationEl_two.value = temp;
+   calculate()
+ })
 
-// calculate()
+ calculate()
